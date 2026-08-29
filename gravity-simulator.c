f57c810ae5d793b291545f6ed6a7d7db3132dc88
nickstewart <nick.stewart1993@gmail.com>
2026-08-29 21:11:24 +0100

    correct compute forces to be semi euler

--- gravity-simulator.c
+++ gravity-simulator.c
@@ -47,25 +47,31 @@ int main() {
 
 void compute_forces(Object *objects, int n, double G, double eps2, int dt) {
     for (int i = 0; i < n; i++) {
-        double ax = 0, ay = 0; 
-        for (int j = 0; j < n; j++) {
-            if (i == j) {
-                continue;
-            }
+      double ax = 0, ay = 0; 
+      for (int j = 0; j < n; j++) {
+          if (i == j) {
+              continue;
+          }
 
-            double dx = objects[j].x - objects[i].x;
-            double dy = objects[j].y - objects[i].y; 
-            double r2 = dx*dx + dy*dy + eps2;
-            double inv_r3 = 1.0 / (r2 * sqrt(r2));
-            double f = G * objects[j].mass * inv_r3;
-            ax += f * dx;
-            ay += f * dy;
-        }
-
-        objects[i].vx += ax;
-        objects[i].vy += ay;
-        objects[i].x = objects[i].x + objects[i].vx * dt;
-        objects[i].y = objects[i].y + objects[i].vy * dt;
+          double dx = objects[j].x - objects[i].x;
+          double dy = objects[j].y - objects[i].y; 
+          double r2 = dx*dx + dy*dy + eps2;
+          double inv_r3 = 1.0 / (r2 * sqrt(r2));
+          double f = G * objects[j].mass * inv_r3;
+          ax += f * dx;
+          ay += f * dy;
+      }
+      objects[i].vx += ax;
+      objects[i].vy += ay;
     }
 
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == j) {
+            continue;
+        }
+        objects[i].x = objects[i].x + objects[i].vx * dt;
+        objects[i].y = objects[i].y + objects[i].vy * dt;
+      }
+    }
 }
